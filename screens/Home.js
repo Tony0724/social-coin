e24c2c2ac232a69bb5ac9ef17ec55a21d3c7e2a4
sungminkim <0724ksm@naver.com>
2023-09-11 20:51:18 +0900

    first commit

---
+++ screens/Home.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components/native";
+import { BLACK_COLOR } from "../colors";
+import { ActivityIndicator, FlatList } from "react-native";
+import { useQuery } from "react-query";
+import { coins } from "../api";
+
+const Container = styled.View`
+  background-color: ${BLACK_COLOR};
+  flex: 1;
+`;
+const Loader = styled.View`
+  flex: 1;
+  background-color: ${BLACK_COLOR};
+  justify-content: center;
+  align-items: center;
+`;
+
+const Coin = styled.View`
+  align-items: center;
+`;
+const CoinName = styled.Text`
+  color: white;
+`;
+const CoinSymbol = styled.Text`
+  color: white;
+`;
+
+const Home = () => {
+  const {isLoading, data} = useQuery('coins', coins);
+  const [cleanData, setCleanData] = useState([]);
+  useEffect(() => {
+    if (data) {
+      setCleanData(
+        data.filter((coin) => coin.rank != 0 && coin.is_active && !coin.is_new)
+      );
+    }
+  }, [data]);
+  if(isLoading) {
+    return <Loader><ActivityIndicator color='white' size={"large"} /></Loader>
+  }
+  return (
+    <Container>
+      <FlatList 
+        data={cleanData}
+        numColumns={3}
+        keyExtractor={(item) => item.id}
+        renderItem={({item}) => <Coin><CoinName>{item.name}</CoinName><CoinSymbol>{item.symbol}</CoinSymbol></Coin>}
+      />
+    </Container>
+  )
+};
+export default Home;
